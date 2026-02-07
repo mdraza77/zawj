11db2e7c07666151ec891ab92a5d1c8b00998fb9
Md Raza <mdraza8397@gmail.com>
2026-02-08 00:00:49 +0530

    Some minor changes

--- app/Http/Controllers/Front/DashboardController.php
+++ app/Http/Controllers/Front/DashboardController.php
@@ -13,14 +13,17 @@ class DashboardController extends Controller
     {
         $user = Auth::user();
 
-        $genderToFind = ($user->gender == 'male') ? 'female' : 'male';
+// Opposite gender logic
+$genderToFind = ($user->gender == 'male') ? 'female' : 'male';
 
-        $suggestedMatches = User::where('id', '!=', $user->id)
-            // ->where('gender', $genderToFind)
-            // ->where('is_verified', true)
-            ->limit(5)
-            ->get();
+$suggestedMatches = User::where('id', '!=', $user->id)
+    ->where('gender', $genderToFind)
+    // ->where('is_verified', true)
+    // Yahan hum Admin aur Super Admin ko exclude kar rahe hain
+    ->withoutRole(['admin', 'Super Admin']) 
+    ->limit(5)
+    ->get();
 
-        return view('front.dashboard', compact('user', 'suggestedMatches'));
+return view('front.dashboard', compact('user', 'suggestedMatches'));
     }
 }
